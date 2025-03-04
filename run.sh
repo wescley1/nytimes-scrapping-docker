5d7b7caa79860b0325d8732cc2713b99a367d1b6
wescley1 <wescleydecarvalho@hotmail.com>
2025-03-04 17:21:51 -0300

    Adiciona script run.sh para executar o container docker

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Variáveis de ambiente
+SEARCH_PHRASE="${SEARCH_PHRASE:-"liverpool"}"
+CATEGORIES="${CATEGORIES:-"Sports"}"
+MONTHS="${MONTHS:-10}"
+SORTING="${SORTING:-"newest"}"
+
+# Diretório de resultados
+RESULTADOS_DIR="${RESULTADOS_DIR:-"./resultados/output"}"
+
+# Cria o diretório de resultados se ele não existir
+mkdir -p "$RESULTADOS_DIR"
+
+# Executa o container Docker
+docker run \
+    -e SEARCH_PHRASE="$SEARCH_PHRASE" \
+    -e CATEGORIES="$CATEGORIES" \
+    -e MONTHS="$MONTHS" \
+    -e SORTING="$SORTING" \
+    -v "$(pwd)/$RESULTADOS_DIR:/nytimes-scrapping/resultados" \
+    ghcr.io/wescley1/nytimes-scrapping-docker/one4tech:latest
