6cc5d2e8f229439b3fc342b164aa3267845892c4
wescley1 <wescleydecarvalho@hotmail.com>
2025-03-04 18:35:11 -0300

    Atualizado run.sh para refletir corretamente o mapeamento dos volumes

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ MONTHS="${MONTHS:-10}"
 SORTING="${SORTING:-"newest"}"
 
 # Diretório de resultados
-RESULTADOS_DIR="${RESULTADOS_DIR:-"./resultados/output"}"
+RESULTADOS_DIR="${RESULTADOS_DIR:-"./resultados"}"
 
 # Cria o diretório de resultados se ele não existir
 mkdir -p "$RESULTADOS_DIR"
@@ -18,6 +18,6 @@ docker run \
     -e CATEGORIES="$CATEGORIES" \
     -e MONTHS="$MONTHS" \
     -e SORTING="$SORTING" \
-    -v "$(pwd)/$RESULTADOS_DIR:/projeto/resultados" \
+    -v "$(pwd)/$RESULTADOS_DIR:/nytimes-scrapping/resultados" \
     -v "$(pwd)/.env:/projeto/.env" \
     ghcr.io/wescley1/nytimes-scrapping-docker/one4tech:latest
